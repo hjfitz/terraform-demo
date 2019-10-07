7dc3ff6dcb8ff27943ddb4ee6706dd27385779a3
Harry F <harry.f@elanco.com>
2019-10-07 15:24:41 +0100

    thonk

--- azure.tf
+++ azure.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
 	version = "=1.27.0"
+	subscription_id = ""
+	client_id = "${var.AZURE_CLIENT_ID}"
+	client_secret = "${var.AZURE_CLIENT_SECRET}"
+	tenant_id = ""
 }
 
 resource "azurerm_resource_group" "myrg" {
