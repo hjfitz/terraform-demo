1f3c5de6e86564b9b96818c381e76423906639a5
Harry F <harry.f@elanco.com>
2019-10-08 07:56:35 +0100

    test commit

--- azure.tf
+++ azure.tf
@@ -1,12 +1,25 @@
 provider "azurerm" {
-	version = "=1.27.0"
-	subscription_id = ""
-	client_id = "${var.AZURE_CLIENT_ID}"
-	client_secret = "${var.AZURE_CLIENT_SECRET}"
-	tenant_id = ""
+	version 		= "=1.27.0"
 }
 
 resource "azurerm_resource_group" "myrg" {
 	name		= "a_rg_made_with_tf"
 	location	= "uksouth"
+}
+
+resource "azurerm_app_service_plan" "test-deploy" {
+	name				= "test-app-serice-plan-deployment"
+	location			= "${azurerm_resource_group.myrg.location}"
+	resource_group_name	= "${azurerm_resource_group.myrg.name}"
+	sku {
+		tier = "Standard"
+		size = "S1"
+	}
+}
+
+resource "azurerm_app_service" "tf-deployed-aps" {
+	name                = "example-app-service"
+	location            = "${azurerm_resource_group.myrg.location}"
+	resource_group_name = "${azurerm_resource_group.myrg.name}"
+	app_service_plan_id = "${azurerm_app_service_plan.test-deploy.id}"
 }
